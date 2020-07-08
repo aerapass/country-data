0c36347ef929afd082aef46bd0d98968c966c3bc
unknown <you@example.com>
2020-07-08 14:21:06 +0700

    chore(country-data): fix commented code

--- test/calling-codes.spec.ts
+++ test/calling-codes.spec.ts
@@ -1,13 +1,13 @@
 import { countries } from '../src/index'
 import * as _ from 'underscore'
 
-// _.each(countries, (country) => {
-//     if (country.countryCallingCodes && country.countryCallingCodes.length) {
-//         test('should contain codes for ' + country.name, () => {
-//             expect(country.countryCallingCodes).toBeDefined()
-//         })
-//     }
-// })
+_.each(countries, (country) => {
+    if (country.countryCallingCodes && country.countryCallingCodes.length) {
+        test('should contain codes for ' + country.name, () => {
+            expect(country.countryCallingCodes).toBeDefined()
+        })
+    }
+})
 
 test('should contain countries with calling codes', () => {
     const country = countries.all.find((country) => country.name === 'Germany')
